a62aea70c4409fd79f11213d92108e0d59709f33
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-04 16:37:43 +0300

    Functions

---
+++ Functions/FirstLarger.js
@@ -0,0 +1,21 @@
+// Write a function that returns the index of the first element in array that is larger than its neighbours or -1, if there’s no such element.
+
+
+var arr = [11,35,11,11,5],
+i,
+l,
+result;
+function myFunction(arr){
+	for(i=0,l=arr.length;i<=l;i+=1){
+
+		if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+			result=i;
+			console.log('Position of bigger number:' + ' ' + result);
+			return result;
+		}   
+	}
+	if (result===undefined) {
+		console.log('-1');
+	}
+}
+myFunction(arr);
---
+++ Functions/ReverseNumber.js
@@ -0,0 +1,15 @@
+//Write a function that reverses the digits of given decimal number.
+var num,
+    result;
+function  ReverseNum(num){
+	if(num){
+	num = num.toString();
+	num = num.split("").reverse().join("");
+	result=num;
+	console.log(result);
+}  else if(isNaN){
+	console.log('Invalid number');
+} 
+
+}
+ ReverseNum(159.12);
