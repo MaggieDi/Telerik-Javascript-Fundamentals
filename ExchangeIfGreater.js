1398bd5529481e844660bc521f4310a83695153c
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-04 15:03:31 +0300

    Exchange greater number

---
+++ ExchangeIfGreater.js
@@ -0,0 +1,11 @@
+// Write an if statement that takes two double variables a and b and exchanges their values if the first one is greater than the second.
+//    As a result print the values a and b, separated by a space.
+
+var a = 5,
+	b = 2;
+
+if (a<b) {
+	console.log(a + ' ' + b) ;
+	} else {
+		console.log(b + ' ' + a);
+	}
