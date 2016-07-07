dcbb7e81ad699f66b48661c2ab6fb08e4eb1b53c
MaggieDi <maggie23ilieva@gmail.com>
2016-07-07 12:11:56 +0300

    Update MaxIncreasingSequence.js

--- Arrays/MaxIncreasingSequence.js
+++ Arrays/MaxIncreasingSequence.js
@@ -1,22 +1,19 @@
-	//Write a script that finds the maximal increasing sequence of equal elements in an array.
-		var arr = [3, 1, 2, 3, 4, 2, 2, 4, 1, 2, 3,5,6],
-			temp = [],
-			result = [];
+//Write a script that finds the maximal increasing sequence of equal elements in an array.
+	var arr = [3, 1, 2, 3, 4, 2, 2, 4, 1, 2, 3,5,6],
+		temp = [],
+		result = [];
 
-		for(var i =0, n =arr.length; n >= i; i+=1)
+	for(var i =0, n =arr.length; n >= i; i+=1)
 { 
-   		 if (arr[i] < arr[i+1] || (arr[i] > arr[i-1] && i == n-1))
-    	{
+   		if (arr[i] < arr[i+1] || (arr[i] > arr[i-1] && i == n-1))
+   		{
         		temp.push(arr[i]);
-    		} 
-    	else if (arr[i] > arr[i+1]) 
-    	{
-        temp = [];
-   		 }
-    	if(result.length < temp.length)
-    {
+    		} else if (arr[i] > arr[i+1]){
+        		temp = [];
+   		}
+    		if(result.length < temp.length){
         
-        result = temp;
-    }
+        		result = temp;
+    		}
 }
 console.log(result);
