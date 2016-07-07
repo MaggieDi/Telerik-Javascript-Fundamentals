9635af4452aa83808685da031535851d17777140
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-07 14:15:45 +0300

    Arrays

---
+++ Arrays/SortingNumbers.js
@@ -0,0 +1,19 @@
+//Write a script to sort an array.
+//Use the selection sort algorithm: Find the smallest element, move it at the first position, find the smallest from the rest, move it at the second position, etc.
+var arr =[91,28,59,60,30,18,4,7,6,15],
+
+
+        if (arr[i] > arr[j]) 
+        {
+            temp = arr[i];
+           
+            arr[i] = arr[j];
+            
+            arr[j] = temp;
+            
+        }
+       
+    }
+}
+
+
