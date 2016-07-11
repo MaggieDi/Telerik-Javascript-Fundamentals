7944691d71b4744bcc757b8adbe39d0d3414aa26
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-11 16:47:41 +0300

    Conditional statements

--- Arrays/SortingNumbers.js
+++ Arrays/SortingNumbers.js
@@ -1,7 +1,13 @@
 //Write a script to sort an array.
 //Use the selection sort algorithm: Find the smallest element, move it at the first position, find the smallest from the rest, move it at the second position, etc.
 var arr =[91,28,59,60,30,18,4,7,6,15],
+	i,
+	j,
+	l,
+	temp;
+for (i = 0, l=arr.length; i < l; i += 1) {
 
+    for (j = i + 1, l=arr.length; j < l; j += 1) {
 
         if (arr[i] > arr[j]) 
         {
@@ -16,4 +22,5 @@ var arr =[91,28,59,60,30,18,4,7,6,15],
     }
 }
 
+console.log('Sorted array is ' + arr.join(', '));
 
