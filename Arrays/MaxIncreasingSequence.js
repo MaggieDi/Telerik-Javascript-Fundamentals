3aa642f7660450ccbe1ef4c1ca4a01d2632b1cad
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-18 16:31:56 +0300

    fixing tabulation

--- Arrays/MaxIncreasingSequence.js
+++ Arrays/MaxIncreasingSequence.js
@@ -5,15 +5,15 @@
 
 	for(var i =0, n =arr.length; n >= i; i+=1){
 
-   	if (arr[i] < arr[i+1] || (arr[i] > arr[i-1] && i == n-1)){
-      temp.push(arr[i]);
+    	if (arr[i] < arr[i+1] || (arr[i] > arr[i-1] && i == n-1)){
+          temp.push(arr[i]);
 
-    } else if (arr[i] > arr[i+1]){
-      temp = [];
-   	}
+      } else if (arr[i] > arr[i+1]){
+          temp = [];
+   	  }
     
-    if(result.length < temp.length){
+      if(result.length < temp.length){
       result = temp;
-    }
-}
-console.log(result);
+      }
+  }
+  console.log(result);
