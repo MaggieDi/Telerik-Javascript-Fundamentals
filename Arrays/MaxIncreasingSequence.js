7c1fdfad9c839d15ad8d5e2f74d87cb504db0ba1
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-18 16:46:08 +0300

    fixing tabulation

--- Arrays/MaxIncreasingSequence.js
+++ Arrays/MaxIncreasingSequence.js
@@ -1,19 +1,18 @@
 //Write a script that finds the maximal increasing sequence of equal elements in an array.
-	var arr = [3, 1, 2, 3, 4, 2, 2, 4, 1, 2, 3,5,6],
-		  temp = [],
-		  result = [];
+var arr = [3, 1, 2, 3, 4, 2, 2, 4, 1, 2, 3,5,6],
+	temp = [],
+	result = [];
 
-	for(var i =0, n =arr.length; n >= i; i+=1){
+for(var i =0, n =arr.length; n >= i; i+=1){
+  if (arr[i] < arr[i+1] || (arr[i] > arr[i-1] && i == n-1)){
+      temp.push(arr[i]);
 
-    	if (arr[i] < arr[i+1] || (arr[i] > arr[i-1] && i == n-1)){
-          temp.push(arr[i]);
-
-      } else if (arr[i] > arr[i+1]){
-          temp = [];
-   	  }
-    
-      if(result.length < temp.length){
-      result = temp;
-      }
+  } else if (arr[i] > arr[i+1]){
+      temp = [];
   }
+    
+  if(result.length < temp.length){
+      result = temp;
+  }
+}
   console.log(result);
