f2313bb99dbca5b41c05c0d90457b9fa85ef49ba
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-18 16:51:36 +0300

    fixing tabulation

--- Functions/FirstLarger.js
+++ Functions/FirstLarger.js
@@ -6,11 +6,11 @@ var arr = [11,35,11,11,5],
 	l,
 	result;
 function myFunction(arr){
- for(i=0,l=arr.length;i<=l;i+=1){
-		if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-		 result=i;
-		 console.log('Position of bigger number:' + ' ' + result);
-		 return result;
+  for(i=0,l=arr.length;i<=l;i+=1){
+	if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+		result=i;
+		console.log('Position of bigger number:' + ' ' + result);
+		return result;
 		}   
 	}
  if (result===undefined) {
