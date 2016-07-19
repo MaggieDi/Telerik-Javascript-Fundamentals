74cdb962ce56e8b323c199ac41b554f628ef2056
Margarita Dimitrova <maggie23ilieva@gmail.com>
2016-07-19 13:32:54 +0300

    fixing tabulation

--- Conditional Statements/quadraticEquation.js
+++ Conditional Statements/quadraticEquation.js
@@ -1,12 +1,12 @@
-var a=-0.5,
-b=4,
-c=-8,
-d=b*b-4*a*c;
+var a = -0.5,
+b = 4,
+c = -8,
+d = b*b-4*a*c;
 
-if(d>0){
+if(d > 0){
 	console.log('First root is: ' + (-b + Math.sqrt(d)) / (2 * a));
 	console.log('Second root is: ' + (-b - Math.sqrt(d)) / (2 * a));
-} else if(d===0){
+} else if(!d){
 	console.log('Root is: ' + (-b/2*a));
 } else{
 	console.log('No real roots.');
